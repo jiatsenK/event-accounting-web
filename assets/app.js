4df08701ae30fb5301567f0e1f097d4efe0687ea
jiatsen.k <jiatsen.k@gmail.com>
2026-08-31 16:01:57 +0800

    Load activity feedback card enhancement

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,3 @@
 document.write('<script src="assets/app-core.js?v=20260831-01"><\/script>');
 document.write('<script src="assets/issue17.js?v=20260831-02"><\/script>');
+document.write('<script src="assets/issue18.js?v=20260831-01"><\/script>');
