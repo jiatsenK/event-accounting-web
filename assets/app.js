1b57bbf94ea1552eace5e8d1e3054ed6276b9ad2
jiatsen.k <jiatsen.k@gmail.com>
2026-08-31 16:09:30 +0800

    Revert accounting dashboard feedback card

--- assets/app.js
+++ assets/app.js
@@ -5,4 +5,3 @@ document.head.appendChild(statsTheme);
 
 document.write('<script src="assets/app-core.js?v=20260831-01"><\/script>');
 document.write('<script src="assets/issue17.js?v=20260831-02"><\/script>');
-document.write('<script src="assets/issue18.js?v=20260831-01"><\/script>');
