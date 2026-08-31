b4f06d17f0dfad811eddba207c5f670a9583baad
jiatsen.k <jiatsen.k@gmail.com>
2026-08-31 16:05:05 +0800

    style: load locked stats theme

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,8 @@
+const statsTheme = document.createElement('link');
+statsTheme.rel = 'stylesheet';
+statsTheme.href = 'assets/stats-theme.css?v=20260831-01';
+document.head.appendChild(statsTheme);
+
 document.write('<script src="assets/app-core.js?v=20260831-01"><\/script>');
 document.write('<script src="assets/issue17.js?v=20260831-02"><\/script>');
 document.write('<script src="assets/issue18.js?v=20260831-01"><\/script>');
